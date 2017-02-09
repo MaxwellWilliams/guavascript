e643b5d093b5e2936eb9d43e89f460a8dfc90c2c
Max Williams <mwill104@lion.lmu.edu>
2017-02-08 19:31:47 -0800

    Added outline for testing parser on example files

--- test/parser/guavascript.ohm.js
+++ test/parser/guavascript.ohm.js
@@ -1,9 +1,30 @@
+fs = require('fs');
+path = require('path');
+ohm = require('ohm-js');
+parserLocation = path.resolve('./guavascript.ohm');
+parserFile = fs.readFileSync(parserLocation, encoding='utf-8');
+parser = ohm.grammar(parserFile);
 assert = require('assert');
+validPrograms = path.resolve('./test/parser/programs/valid');
 
 (function() {
   describe('guavascript.ohm', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+    describe('Test valid example programs', function() {
+      fs.readdirSync(validPrograms).forEach(function(file) {
+        console.log(file);
+        console.log(parser.match(file));
+
+        m = parser.match('myVariable = 10');
+        if (m.succeeded()) {
+          console.log('Success!');
+        } else {
+          console.log('Nope');
+        }
+      });
+      // it('');
     });
   });
 }());

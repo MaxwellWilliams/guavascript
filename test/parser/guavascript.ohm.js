2e888d11933fd1365cc7857e390d18f40221da00
Max Williams <mwill104@lion.lmu.edu>
2017-02-09 01:15:24 -0800

    Added parser testing framework for invalid programs

--- test/parser/guavascript.ohm.js
+++ test/parser/guavascript.ohm.js
@@ -1,20 +1,31 @@
 fs = require('fs');
 path = require('path');
 ohm = require('ohm-js');
+assert = require('assert');
 parserContents = fs.readFileSync('guavascript.ohm');
 parser = ohm.grammar(parserContents);
-assert = require('assert');
 validPrograms = path.resolve('./test/parser/programs/valid');
-async = require('async');
+invalidPrograms = path.resolve('./test/parser/programs/invalid');
 
-tests = function(validFiles) {
+tests = function(validFiles, invalidFiles) {
   describe('guavascript.ohm', function() {
     describe('Test valid example programs', function() {
       validFiles.forEach(function(file) {
-        it(file.name + ' should be accepted by the parser', function() {
+        it('valid\\' +file.name + ' should be accepted by the parser',
+        function() {
           parseResult = parser.match(file.code);
           assert(parseResult.succeeded(),
-            'Is ' + parseResult.succeeded());
+            file.name + ' was ' + parseResult.succeeded());
+        });
+      });
+    });
+    describe('Test invalid example programs', function() {
+      invalidFiles.forEach(function(file) {
+        it('invalid\\' + file.name + ' should be rejected by the parser',
+        function() {
+          parseResult = parser.match(file.code);
+          assert(!parseResult.succeeded(),
+            file.name + ' was ' + parseResult.succeeded());
         });
       });
     });
@@ -23,6 +34,7 @@ tests = function(validFiles) {
 
 (function() {
   validFiles = [];
+  invalidFiles = [];
   fs.readdirSync(validPrograms).forEach(function(fileName) {
     fullFilePath = validPrograms + '/' + fileName;
     fileContents = fs.readFileSync(fullFilePath, 'utf-8');
@@ -31,6 +43,13 @@ tests = function(validFiles) {
       code: fileContents,
     });
   });
-  console.log(validFiles);
-  tests(validFiles);
+  fs.readdirSync(invalidPrograms).forEach(function(fileName) {
+    fullFilePath = invalidPrograms + '/' + fileName;
+    fileContents = fs.readFileSync(fullFilePath, 'utf-8');
+    invalidFiles.push({
+      name: fileName,
+      code: fileContents,
+    });
+  });
+  tests(validFiles, invalidFiles);
 }());

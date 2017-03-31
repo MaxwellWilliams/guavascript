85749eb786799d95b8f19ed981e6763941c74dff
Evan Hessler <ehessler@lion.lmu.edu>
2017-03-31 15:43:00 -0700

    finish diagrams, fix css

--- js/railroad_insert.js
+++ js/railroad_insert.js
@@ -4,7 +4,7 @@ railroad_insert = ((code, title) => {
       <div class="row horizontal-center">
         <div class="col-xs-12 text-center">
           <div class="syntaxdiagrams">
-            <h5 class="text-center">${title}</h5><script>${code}</script>
+            <h4 class="text-center">${title}</h4><script>${code}</script>
           </div>
         </div>
       </div>

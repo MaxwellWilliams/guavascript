8dedf806505ce406d739ab833a7d084d88339077
Evan Hessler <ehessler@lion.lmu.edu>
2017-03-31 12:56:12 -0700

    add template for inserting syntax tree

---
+++ js/railroad_insert.js
@@ -0,0 +1,13 @@
+railroad_insert = ((code) => {
+	return `<div class="one-third examples">
+    <div class="container example vertical-center">
+      <div class="row horizontal-center">
+        <div class="col-xs-12 text-center">
+          <div class="syntaxdiagrams">
+            <h5 class="text-center">Block</h5><script>${code}</script>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+});

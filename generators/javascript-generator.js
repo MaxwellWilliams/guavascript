be99b973178eb830e12c7e53acb0983368ad6bba
Evan Hessler <ehessler@lion.lmu.edu>
2017-04-21 14:39:10 -0700

    import files for generator

--- generators/javascript-generator.js
+++ generators/javascript-generator.js
@@ -1,2 +1,42 @@
 const parser = require('../parser.js');
 
+const Program = require('../entities/program.js');
+const Block = require('../entities/block.js');
+const BranchStatement = require('../entities/branchStatement.js');
+const FunctionDeclarationStatement = require('../entities/functionDeclarationStatement.js');
+const ClassDeclarationStatement = require('../entities/classDeclarationStatement.js');
+const WhileStatement = require('../entities/whileStatement.js');
+const ForInStatement = require('../entities/forInStatement.js');
+const PrintStatement = require('../entities/printStatement.js');
+const AssignmentStatement = require('../entities/assignmentStatement.js');
+const ReturnStatement = require('../entities/returnStatement.js');
+const MatchExpression = require('../entities/matchExpression.js');
+const Match = require('../entities/match.js');
+const Parameter = require('../entities/parameter.js');
+const BinaryExpression = require('../entities/binaryExpression.js');
+const UnaryExpression = require('../entities/unaryExpression.js');
+const ParenthesisExpression = require('../entities/parenthesisExpression.js');
+const IdExpression = require('../entities/idExpression.js');
+const IdExpressionBodyRecursive = require('../entities/idExpressionBodyRecursive.js');
+const IdExpressionBodyBase = require('../entities/idExpressionBodyBase.js');
+const PeriodId = require('../entities/periodId.js');
+const Arguments = require('../entities/arguments.js');
+const IdSelector = require('../entities/IdSelector.js');
+const List = require('../entities/list.js');
+const Tuple = require('../entities/tuple.js');
+const Dictionary = require('../entities/dictionary.js');
+const IdValuePair = require('../entities/idValuePair.js');
+const VarList = require('../entities/varList.js');
+const BoolLit = require('../entities/boolLit.js');
+const IntLit = require('../entities/intLit.js');
+const FloatLit = require('../entities/floatLit.js');
+const StringLit = require('../entities/stringLit.js');
+const NullLit = require('../entities/nullLit.js');
+const IdVariable = require('../entities/idVariable.js');
+const ConstId = require('../entities/constId.js');
+const ClassId = require('../entities/classId.js');
+
+
+module.exports = (program) => {
+	program.gen();
+}

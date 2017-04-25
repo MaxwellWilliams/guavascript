d1efbfe3c6cc6ccdf5b388d65a0925cb2d59c80a
Max Williams <mwill104@lion.lmu.edu>
2017-04-24 22:08:17 -0700

    Fixed indentation

--- test/generator/output/valid/bigProgram.js
+++ test/generator/output/valid/bigProgram.js
@@ -1,72 +1,72 @@
 module.exports.getOutput = function() {
   return `class Test {
 
-    constructor (param1 = 1, param2 = "2", param3 = false, param4 = 0) {
-      this.param1 = param1;
-      this.param2 = param2;
-      this.param3 = param3;
-      this.PARAM4 = param4;
+  constructor (param1 = 1, param2 = "2", param3 = false, param4 = 0) {
+    this.param1 = param1;
+    this.param2 = param2;
+    this.param3 = param3;
+    this.PARAM4 = param4;
+  }
+
+  var getParam1 = () => {
+    return this.param1;
+  }
+
+  var setParam1 = (newParam) = () => {
+    this.param1 = newParam;
+  }
+
+  var checkIfEqual = (paramX, paramY) = () => {
+    return paramX === paramY;
+  }
+
+  class Sub {
+    constructor (x, y, z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
     }
 
-    var getParam1 = () => {
-      return this.param1;
+    var checkVariables = (var1) = () => {
+      var answer;
+      if (var1 != null) {
+        answer = (() {
+          if (var1 === 1) {
+            var1 = "one";
+          } else if (var1 = 2) {
+            var1 = "two";
+          } else {
+            var1 = "not one or two";
+          }
+        })();
+      }
+      return answer;
+    }
+  }
+
+  class Collections {
+
+    constructor (dict, list, tup) {
+      this.dict = dict;
+      this.list = list;
+      this.tup = tup;
     }
 
-    var setParam1 = (newParam) = () => {
-      this.param1 = newParam;
+    var addToDict (id, value) = () => {
+      this.dict[id] = value;
     }
 
-    var checkIfEqual = (paramX, paramY) = () => {
-      return paramX === paramY;
+    var getDict (id) = () => {
+      return this.dict[id];
     }
 
-    class Sub {
-      constructor (x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-      }
+    // var addToList (value) = () => {
+    //    this.list.append(value, 1, "truelalala")
+    // }
 
-      var checkVariables = (var1) = () => {
-        var answer;
-        if (var1 != null) {
-          answer = (() {
-            if (var1 === 1) {
-              var1 = "one";
-            } else if (var1 = 2) {
-              var1 = "two";
-            } else {
-              var1 = "not one or two";
-            }
-          })();
-        }
-        return answer;
-      }
+    var getTuple = () = () => {
+      return this.tup;
     }
-
-    class Collections {
-
-      constructor (dict, list, tup) {
-        this.dict = dict;
-        this.list = list;
-        this.tup = tup;
-      }
-
-      var addToDict (id, value) = () => {
-        this.dict[id] = value;
-      }
-
-      var getDict (id) = () => {
-        return this.dict[id];
-      }
-
-      // var addToList (value) = () => {
-      //    this.list.append(value, 1, "truelalala")
-      // }
-
-      var getTuple = () = () => {
-        return this.tup;
-      }
-    }
-  }`;
+  }
+}`;
 };
--- test/generator/output/valid/class1.js
+++ test/generator/output/valid/class1.js
@@ -1,11 +1,11 @@
 module.exports.getOutput = function() {
   return `class Ball {
-    constructor (radius, weight = 1.0) {
-      this.radius = radius;
-      this.weight = weight;
-    }
-    var is_round = () => {
-      return true;
-    }
-  }`;
+  constructor (radius, weight = 1.0) {
+    this.radius = radius;
+    this.weight = weight;
+  }
+  var is_round = () => {
+    return true;
+  }
+}`;
 };

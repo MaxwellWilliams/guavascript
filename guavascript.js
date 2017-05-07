286937d492389c1358e19ae5e07529abad11a068
Max Williams <mwill104@lion.lmu.edu>
2017-05-06 17:20:58 -0700

    Added optimize option

--- guavascript.js
+++ guavascript.js
@@ -4,9 +4,10 @@ const jsGenerator = require('./generators/javascript-generator.js');
 
 const argv = require('yargs')
     .usage('$0 [-a] [-s] [--js] filename')
-    .boolean(['a', 's', 'js'])
+    .boolean(['a', 's', 'o', 'js'])
     .describe('a', 'parse, then generate an AST')
     .describe('s', 'perform semantic analysis')
+    .describe('o', 'perform optimizations')
     .describe('js', 'generate program in JavaScript')
     .demand(1)
     .argv;
@@ -19,6 +20,9 @@ node guavascript.js -a <filename>
 node guavascript.js -s <filename>
     The '-s' option runs a complete semantic analysis and prints errors, if any.
 
+node guavascript.js -o <filename>
+    The '-o' option runs a set of code optimizaions.
+
 node guavascript.js --js <filename>
     The '--js' option generates the program in JavaScript after running semantic and grammar checks.
 
@@ -26,12 +30,15 @@ node guavascript.js --js <filename> | node
     When using the '--js' option, the output code can be piped to node to be run.`;
 
 fs.readFile(argv._[0], 'utf-8', (err, text) => {
-  const program = parser(text);
+  let program = parser(text);
   if (argv.a) {
     console.log(program.toString());
     return;
   } if (argv.s || argv.js) {
     program.analyze();
+    if (argv.o) {
+      program = program.optimize();
+    }
     if (argv.js) {
       const baseCode = jsGenerator(program);
       console.log(baseCode);
